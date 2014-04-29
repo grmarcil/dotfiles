61b79effbefcdca1178381e104bf9c66c0444db9
Greg Marcil <greg.marcil@logik.com>
2014-04-29 17:19:20 -0400

    Fixed sublime install for osx

--- sublime/install.sh
+++ sublime/install.sh
@@ -5,7 +5,7 @@ ignore=( install.sh )
 if [[ "$OSTYPE" == "linux"* ]]; then
   TARGET="$HOME/.config/sublime-text-3/Packages/User"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  TARGET="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+  TARGET="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 fi
 
 read -r -p "Are you sure? This will delete and replace your Sublime Text 3 user settings [y/N] " response
